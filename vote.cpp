d57119c3097282baa7f57cd8ab3da73ebfde6fba
Abhishekjha2011 <114256000+Abhishekjha2011@users.noreply.github.com>
2022-12-24 19:17:04 +0530

    Finding Eligible For Vote

---
+++ vote.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+		cout<<"Enter the age =";
+		cin>>age;
+	
+	if (age>=18)
+	{
+	
+	cout<<"\n you are Eligible for Voting"<<endl;
+}
+	
+	else 
+	{
+	
+		cout<<"\n you are not Eligible for Voting "<<endl;
+}
+     return 0;	
+	
+	
+}
